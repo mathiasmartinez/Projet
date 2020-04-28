c23cedacbdde77697763bae9e1b2a3f28750a7a7
mathiasmartinez <mathias.martinez@student.isae-supaero.fr>
2020-04-28 11:14:35 +0200

    Creation des tables Task et status

--- Projet/taskmanager/models.py
+++ Projet/taskmanager/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
@@ -7,4 +8,27 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+class Status(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    project = models.ForeignKey('Project',on_delete = models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    assignee = models.ForeignKey(User,on_delete=models.CASCADE)
+    start_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeField(default=timezone.now)
+    priority = models.IntegerField()
+    status = models.ForeignKey('Status', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 
