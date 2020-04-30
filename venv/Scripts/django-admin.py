e9133a03536a6c33f575ee5e4f8228f4dc5ca61c
mathiasmartinez <mathias.martinez@student.isae-supaero.fr>
2020-04-30 08:40:09 +0200

    Suppression fichiers venv et idea

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\mathi\desktop\cours\appli_web_nouvelle_generation\projet\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
